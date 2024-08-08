cced9517e96b52bf5025d78d00408f3c3da007ff
Khang Nguyen <ngkhang0220@gmail.com>
2024-08-08 11:44:08 +0700

    setup: add Husky, Lint-staged and CommitLint

---
+++ .lintstagedrc.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) => {
+  return `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')
+    }`;
+}
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [
+    buildEslintCommand
+  ],
+};
